8ae482d15df017e937adf1d60dccd09dab3daf67
saikamalkola <saikamalkola@gmail.com>
2017-10-04 14:30:39 +0530

    Added 8x8 font and 7x5 font to the code with fonts stored in PROGMEM of arduino

--- matrix_8x64/matrix_8x64.ino
+++ matrix_8x64/matrix_8x64.ino
@@ -4,13 +4,14 @@
         Multiplexing( No timer Interrypts )
 
   code by:
-  K S S M KAMAL
+  K S S M KAMALK
   NIT SURATHKAL
   Dept of ECE
 */
 
 #include <SPI.h>
 #include "fonts.h"
+#include <avr/pgmspace.h>
 
 #define clock_pin 13  //clock pin for 74HC595
 #define latch_pin 2   //latch pin for 74HC595
@@ -21,24 +22,24 @@
 
 
 unsigned long pause_time = 3000000;
-unsigned long scroll_speed = 40000;
+unsigned long scroll_speed = 60000;
 
 unsigned long present_ms = 0;
 unsigned long last_ms = 0;
 unsigned long pause_ms = 0;
 
-int scroll_index = 0;
-boolean pause;
+uint8_t fade_column = 0, scroll_index = 0;
+boolean flag = 0, pause;
 boolean scroll_direction = 0;  //0 - scroll left and 1 - scroll right
 
-uint64_t temp, led[8] = {0};  //led array with each element consisting 64 bits which corresponds to row data in multiplexing
+uint64_t bitmask = 1, temp, led[8] = {0}; //led array with each element consisting 64 bits which corresponds to row data in multiplexing
 
 uint64_t scroll_msg[2][8];   //led array with real and virtual matrix data
 unsigned int msg_index = 0;
-
+boolean font_sel = 1; // 0 for 8x8 font, 1 for 7x5 font
 const byte char_set[] = " ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+-,.~:;<>?@[]/\\=_";
 
-String msg = "TECHNITES";
+String msg = "VENKY LOVES HARRY ";
 
 void setup() {
   // put your setup code here, to run once:
@@ -61,7 +62,7 @@ void setup() {
   latch();
   //interrupts();
   msg_check();
-  frame_decode();
+  //frame_decode_7x5();
   initialize();
 }
 
@@ -75,7 +76,7 @@ void loop()
       finish_scroll();
       if (present_ms - pause_ms > pause_time)
       {
-        if(Serial.available() > 0)
+        if (Serial.available() > 0)
         {
           msg = Serial.readStringUntil('\n');
           msg_check();
@@ -86,36 +87,37 @@ void loop()
     else
       scroll_message();
   }
-  scroll_multiplex();
- //multiplex();
+  // scroll_multiplex();
+  multiplex(scroll_msg[0]);
+
 }
 
-void multiplex()
+void multiplex(uint64_t arr[])
 {
   uint8_t row_sel = 128;
   for (int i = 0; i < 8; i++)
   {
-   // SPI_data_transfer(0);
+    // SPI_data_transfer(0);
     //SPI.transfer(0);
     //latch();
-    SPI_data_transfer(led[i]);
+    SPI_data_transfer(arr[i]);
     SPI.transfer(row_sel);
     row_sel = row_sel >> 1;
     latch();
-  } 
+  }
 }
 
 void msg_check()
 {
-  if(msg.length() % 8 != 0)
-     msg += "       ";
+  if (msg.length() % 8 != 0)
+    msg += "             ";
 }
 void scroll_multiplex()
 {
   uint8_t row_sel = 128;
   for (int i = 0; i < 8; i++)
   {
-   // SPI_data_transfer(0);
+    // SPI_data_transfer(0);
     //SPI.transfer(0);
     //latch();
     SPI_data_transfer(scroll_msg[0][i]);
@@ -135,7 +137,13 @@ void initialize()
     }
   }
   msg_index = 0;
-  scroll_frame_decode();
+  if (font_sel == 0)
+  {
+    scroll_frame_decode();
+  }
+  else {
+    frame_decode_7x5();
+  }
   pause = false;
 }
 
@@ -159,7 +167,13 @@ void scroll_message()
   if (scroll_index >= 64)
   {
     scroll_index = 0;
-    scroll_frame_decode();
+    if (font_sel == 0)
+    {
+      scroll_frame_decode();
+    }
+    else {
+      frame_decode_7x5();
+    }
   }
 }
 void scroll_frame_decode()
@@ -192,7 +206,7 @@ void scroll_frame_decode()
   {
     for (int j = 0; j < 8; j++)
     {
-      temp = char_def[char_index[j] * 8 + i];
+      temp = pgm_read_byte_near(char_def + (8 * char_index[j] + i));
       scroll_msg[1][7 - i] |= (temp << (63 - 8 * (j + 1) + 1));
     }
   }
@@ -241,16 +255,11 @@ void latch()
 
 }
 
-void frame_decode_7x5()
-{
-  int char_index[10] = {0};
-}
-
 
 void frame_decode()
 {
   int char_index[8] = {0};
-  
+
   for (int i = 0; i < 8; i++)
   {
     for (int j = 0; j < 89; j++)
@@ -268,12 +277,63 @@ void frame_decode()
   {
     for (int j = 0; j < 8; j++)
     {
-      temp = char_def[char_index[j] * 8 + i];
+      temp = pgm_read_byte_near(char_def + (8 * char_index[j] + i));
       led[7 - i] |= (temp << (63 - 8 * (j + 1) + 1));
     }
   }
 }
 
 //Interrupt service routines
+void frame_decode_7x5()
+{
+  for (int i = 0; i < 8; i++)
+  {
+    scroll_msg[1][i] = 0;
+  }
+  uint8_t num_col = 0, max_char = 0, char_index_7x5[21] = {0};
+  for (int i = 0; i < 21; i++)
+  {
+    if (msg[msg_index] == '\0')
+    {
+      msg_index = 0;
+      pause = true;
+      pause_ms = present_ms;
+    }
+    for (uint8_t j = 0; j < 96; j++)
+    {
+      if (msg[msg_index] == j + 32)
+      {
+        char_index_7x5[i] = j;
+        num_col += pgm_read_byte_near(char_def_7x5 + (8 * j + 7));
+        max_char++;
+        break;
+      }
+    }
+    msg_index++;
+    if (num_col >= 64)
+    {
+      break;
+    }
+  }
+
+
+  scroll_msg[1][7] = 0;
+  for (int i = 6; i >= 0; i--)
+  {
+    uint16_t difference =  63;
+    for (int j = 0; j < max_char; j++)
+    {
+
+      uint8_t shift_num = pgm_read_byte_near(char_def_7x5 + (8 * char_index_7x5[j] + 7));
+      temp = pgm_read_byte_near(char_def_7x5 + (8 * char_index_7x5[j] + i));
+      if (difference < 8)
+        scroll_msg[1][6 - i] |= (temp >> (8 - shift_num));
+      else
+        scroll_msg[1][6 - i] |= temp << (difference - 8 + 1);
+      difference -= shift_num;
+    }
+  }
+}
+
 
 
